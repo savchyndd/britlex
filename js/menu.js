3db1f85d9cd5225856a840274829a0fe3aaca505
Dmytro <savchyndd@gmail.com>
2023-05-14 09:01:59 +0200

    add hero section adaptive

---
+++ js/menu.js
@@ -0,0 +1,18 @@
+(() => {
+  const mobileRefs = {
+    menuBtnRef: document.querySelector('[data-menu-btn]'),
+    mobileMenuRef: document.querySelector('[data-menu]'),
+  };
+
+  mobileRefs.menuBtnRef.addEventListener('click', toggleMobileMenu);
+
+  function toggleMobileMenu() {
+    const expanded = mobileRefs.menuBtnRef.getAttribute('aria-expanded') === 'true' || false;
+    mobileRefs.menuBtnRef.classList.toggle('is-open');
+    mobileRefs.menuBtnRef.setAttribute('aria-expanded', !expanded);
+
+    mobileRefs.mobileMenuRef.classList.toggle('is-open');
+
+    document.body.classList.toggle('mobile-menu-open');
+  }
+})();
